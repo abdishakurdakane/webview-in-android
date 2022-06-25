41d2aebc26643a287e4120eed2ecfd87ecaed24f
shakuur_ali <abdishakur.ali@taajservices.com>
2022-06-25 13:07:24 +0300

    git init

---
+++ app/src/main/java/com/shakuurdev/webviewinandroid/MainActivity.java
@@ -0,0 +1,24 @@
+package com.shakuurdev.webviewinandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.webview);
+        webView.loadUrl("https://jsonplaceholder.typicode.com/posts");
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        // WebViewClient allows you to handle
+        // onPageFinished and override Url loading.
+        webView.setWebViewClient(new WebViewClient());
+    }
+}
